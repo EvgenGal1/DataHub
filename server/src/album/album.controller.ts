14701fdd66c419e4a1ce0100d9eebe49bd4b4ad2
EvGen Gal <genwolk1@gmail.com>
2023-11-21 03:56:40 +0300

    настр.неск.мтд.в окруж. albums

--- server/src/album/album.controller.ts
+++ server/src/album/album.controller.ts
@@ -6,44 +6,69 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 // import { ObjectId } from 'typeorm';
-import { /* ApiBearerAuth, */ ApiTags } from '@nestjs/swagger';
+import { /* ApiBearerAuth, */ ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { AlbumService } from './album.service';
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
 
-@Controller('album')
+@Controller('/albums')
 // групп.мтд.cntrl tracks > swagger
-@ApiTags('album')
+@ApiTags('albums')
 // сообщ.о защищены req jwt Токеном > swagger
 // @ApiBearerAuth()
 export class AlbumController {
-  constructor(private readonly albumService: AlbumService) {}
+  constructor(private readonly albumsService: AlbumService) {}
 
+  // ^^ МТД.CRUD
   @Post()
+  @ApiOperation({ summary: 'Создать Альбом' })
   create(@Body() createAlbumDto: CreateAlbumDto) {
-    return this.albumService.create(createAlbumDto);
+    return this.albumsService.create(createAlbumDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Получить Все Альбомы' })
   findAll() {
-    return this.albumService.findAll();
+    return this.albumsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Получить Альбом' })
   findOne(@Param('id') id: string) {
-    return this.albumService.findOne(+id);
+    return this.albumsService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Обновить Альбом' })
   update(@Param('id') id: string, @Body() updateAlbumDto: UpdateAlbumDto) {
-    return this.albumService.update(+id, updateAlbumDto);
+    return this.albumsService.update(+id, updateAlbumDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Удалить Альбом/ы' })
   remove(@Param('id') id: string) {
-    return this.albumService.remove(+id);
+    return this.albumsService.remove(+id);
+  }
+
+  // ^^ ДОП.МТД.
+  // поиск по исполнителю
+  // ! не раб. @Get('/:author') | '/author' | 'author' > отраб.мтд.@Get(':id')findOne
+  @Get('/album/:author_Name')
+  @ApiOperation({ summary: 'Поиск Альбома по Автору' })
+  searchByAuthor(
+    /* @Param // возвращ.всё */ @Query('author') authorName: string,
+  ) /* : Promise<Album[]> // надо ли тип.возврат. */ {
+    return this.albumsService.searchByAuthor(authorName);
+  }
+
+  // поиск по назв.альбома
+  @Get('/album/:album_Name')
+  @ApiOperation({ summary: 'Поиск Альбома по Названию' })
+  searchByAlbumName(@Query('album') albumName: string) {
+    return this.albumsService.searchByAlbumName(albumName);
   }
 }
--- server/src/album/album.service.ts
+++ server/src/album/album.service.ts
@@ -1,39 +1,54 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { ObjectId, Repository } from 'typeorm';
+import { Like, ObjectId, Repository } from 'typeorm';
 
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
+import { AlbumEntity } from './entities/album.entity';
 import { TrackEntity } from 'src/track/entities/track.entity';
 import { CommentEntity } from 'src/track/entities/comment.entity';
 
 @Injectable()
 export class AlbumService {
   constructor(
+    @InjectRepository(AlbumEntity)
+    private albumsRepository: Repository<AlbumEntity>,
     @InjectRepository(TrackEntity)
-    private trackRepository: Repository<TrackEntity>,
+    private tracksRepository: Repository<TrackEntity>,
     @InjectRepository(CommentEntity)
-    private commentRepository: Repository<CommentEntity>,
+    private commentsRepository: Repository<CommentEntity>,
   ) {}
 
+  // ^^ МТД.CRUD
   create(createAlbumDto: CreateAlbumDto) {
     return 'This action adds a new album';
   }
 
   findAll() {
-    return `This action returns all album`;
+    return `Это действие возвращает весь альбом`;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} album`;
+    return `Это действие возвращает #${id} album`;
   }
 
   update(id: number, updateAlbumDto: UpdateAlbumDto) {
-    return `This action updates a #${id} album`;
+    return `Это действие обновляет #${id} album`;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} album`;
+    return `Это действие удаляет #${id} album`;
+  }
+
+  // ^^ ДОП.МТД.
+  // поиск по исполнителю
+  async searchByAuthor(author: string): Promise<AlbumEntity[]> {
+    return this.albumsRepository.find({ where: { author: author } });
+  }
+
+  // поиск по назв.альбома
+  async searchByAlbumName(album: string): Promise<AlbumEntity[]> {
+    return this.albumsRepository.find({ where: { album: album } });
   }
 }
