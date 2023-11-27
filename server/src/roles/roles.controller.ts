fc43066139d655550d42ccd37f9bb48fef06200f
EvGen Gal <genwolk1@gmail.com>
2023-11-27 05:52:26 +0300

    мини правки. ОЧ.МН.ОБН.в ф. Распред.по лог.,загр.

--- server/src/roles/roles.controller.ts
+++ server/src/roles/roles.controller.ts
@@ -7,11 +7,14 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
 
 @Controller('roles')
+@ApiTags('roles')
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
