1fbc1afa05aaf7580c9e913fdca5953506f85111
EvGen Gal <genwolk1@gmail.com>
2023-12-02 04:41:34 +0300

    $multer > загр.ф.; UserId вр.правки;

--- server/src/decorators/user-id.decorator.ts
+++ server/src/decorators/user-id.decorator.ts
@@ -5,6 +5,9 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const UserId = createParamDecorator(
   (_: unknown, ctx: ExecutionContext): number | null => {
     const request = ctx.switchToHttp().getRequest();
-    return request.user?.id ? Number(request.user.id) : null;
+    return request.user?.id
+      ? Number(request.user.id)
+      : /* // !! врем.1,а !=null */
+        1;
   },
 );
