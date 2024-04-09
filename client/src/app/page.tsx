70945bcf8aaf089a5d6d68c4dec8d0152a70c208
EvGen Gal <genwolk1@gmail.com>
2024-04-09 21:59:42 +0300

    CLT. мини чистка, тесты, -cg

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -8,15 +8,6 @@ export default function Home() {
       <main className="center flex flex-col items-center justify-center p-24">
         <h1>Добро пожаловать</h1>
         <h3>Лучшие Треки</h3>
-        {/* стили по Next // ! не раб.ошб. - ⨯ ./node_modules\next\dist\compiled\client-only\error.js
-            'client-only' cannot be imported from a Server Component module. It should only be used from a Client Component.
-            Import trace for requested module: ..... ./src\app\page.tsx */}
-        {/* <style jsx>
-          {`
-            .center {
-              margin-top: 150px
-          `}
-        </style> */}
       </main>
     </>
   );
