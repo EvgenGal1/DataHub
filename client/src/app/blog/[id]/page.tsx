8fa56142b644fc492b1ffaecc16bd2424b11ea56
EvGen Gal <genwolk1@gmail.com>
2024-04-15 19:18:01 +0300

    CLT. Blog+/id(async, fetch, metaData, рендер)

--- client/src/app/blog/[id]/page.tsx
+++ client/src/app/blog/[id]/page.tsx
@@ -2,16 +2,36 @@
 
 import { Metadata } from "next";
 
+// вспомог.fn получ.данн.(без exp)
+async function getDate(id: string) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+    { next: { revalidate: 60 } }
+  );
+  if (!response.ok) {
+    throw new Error("Пост не получен!");
+  }
+  return response.json();
+}
 // тип парам.
 type Props = { params: { /* по назв.[п.] */ id: string } };
 
-// fn.настр.метадаты > SEO
+// fn.настр.метадаты > SEO + настр.асинхр.отраж.данн.
 export async function generateMetadata({
   params: { id },
 }: Props): Promise<Metadata> {
-  return { title: id };
+  const post = await getDate(id);
+  return { title: post.title };
 }
 
-export default function Post({ params: { id } }: Props) {
-  return <div>Post page {id}</div>;
+export default async function Post({ params: { id } }: Props) {
+  // асинхр.загр.данн.
+  const post = await getDate(id);
+  return (
+    <div className="post">
+      <h1>Post page {id}</h1>
+      <h3>{post.title}</h3>
+      <p>{post.body}</p>
+    </div>
+  );
 }
--- client/src/app/blog/page.tsx
+++ client/src/app/blog/page.tsx
@@ -1,9 +1,38 @@
-"use client";
+import { Metadata } from "next";
+import Link from "next/link";
 
-export default function Blog() {
+// вспомог.fn получ.данн.(без exp). Отраб.на serv,
+async function getDate() {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/posts",
+    // расшир.доп.объ.настр.fetch от Next > повотр.провер.данн.кеша (здесь раз в 60 сек.проверка)
+    { next: { revalidate: 60 } }
+  );
+  if (!response.ok) {
+    throw new Error("Невозможно получить посты!");
+  }
+  return response.json();
+}
+
+// объ.настр.метадаты > SEO
+export const metadata: Metadata = {
+  title: "Blog | Music Platform",
+};
+
+// serv.Комп могут быть async
+export default async function Blog() {
+  // асинхр.загр.данн.
+  const posts = await getDate();
   return (
     <div className="blog">
       <h1>Blog page Blog</h1>
+      <ul>
+        {posts.map((post: any) => (
+          <li key={post.id}>
+            <Link href={`/blog/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
