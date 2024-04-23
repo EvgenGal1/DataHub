616bec99b5c40cdfa2beb261e3a5cdba4a35c794
EvGen Gal <genwolk1@gmail.com>
2024-04-24 01:06:29 +0300

    SRV. Vercel. правки > start

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,7 @@
 // общ.модуль приложения
-import { TypeOrmModule } from '@nestjs/typeorm';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { ConfigModule, ConfigService } from '@nestjs/config';
 // import { ServeStaticModule } from '@nestjs/serve-static';
 // import * as path from 'path';
 
@@ -24,55 +24,55 @@ import { AppService } from './app.service';
 
 // декор.модуль. (организ.структуры области действ.> cntrl и provider)
 @Module({
-  imports: [
-    // подкл.модуль > счит.перем.из.env
-    ConfigModule.forRoot({
-      // путь к ф.конфиг. (по умолч.ищет в корне .env)
-      envFilePath: '.env',
-      // повысить производит.доступа к перем.в process.env.
-      cache: true,
-    }),
-    // подкл.к БД ч/з перем.process.env
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('ELEPHANT_PG_HOST'),
-        port: Number(configService.get('ELEPHANT_PG_PORT')) || 5432,
-        database: configService.get('ELEPHANT_PG_DB'),
-        username: configService.get('ELEPHANT_PG_USER'),
-        password: configService.get('ELEPHANT_PG_PSW'),
-        // entities: [
-        //   UserEntity,
-        //   RoleEntity,
-        //   UserRolesEntity,
-        //   FileEntity,
-        //   TrackEntity,
-        //   AlbumEntity,
-        //   ReactionEntity,
-        // ],
-        // ^^ ТОЛЬКО DEV true
-        // синхрон.табл.БД, логи сборки
-        // ! при true начала падать в ошб. - ERROR [TypeOrmModule] Unable to connect to the database. Retrying (1)... QueryFailedError: максимальное число столбцов в таблице: 1600
-        synchronize: false,
-        logging: false,
-      }),
-      inject: [ConfigService],
-    }),
-    // обслуж.статич.контент по путь/папка ч/з веб-сайт
-    // ServeStaticModule.forRoot({
-    //   rootPath: path.resolve(__dirname, /* '..', */ 'static'),
-    //   serveRoot: '/static',
-    // }),
-    // подкл.использ.modulи
-    // // AuthModule,
-    // UsersModule,
-    // RolesModule,
-    // FilesModule,
-    // TrackModule,
-    // AlbumModule,
-    // ReactionsModule,
-  ],
+  // imports: [
+  //   // подкл.модуль > счит.перем.из.env
+  //   ConfigModule.forRoot({
+  //     // путь к ф.конфиг. (по умолч.ищет в корне .env)
+  //     envFilePath: '.env',
+  //     // повысить производит.доступа к перем.в process.env.
+  //     cache: true,
+  //   }),
+  //   // подкл.к БД ч/з перем.process.env
+  //   TypeOrmModule.forRootAsync({
+  //     imports: [ConfigModule],
+  //     useFactory: (configService: ConfigService) => ({
+  //       type: 'postgres',
+  //       host: configService.get('ELEPHANT_PG_HOST'),
+  //       port: Number(configService.get('ELEPHANT_PG_PORT')) || 5432,
+  //       database: configService.get('ELEPHANT_PG_DB'),
+  //       username: configService.get('ELEPHANT_PG_USER'),
+  //       password: configService.get('ELEPHANT_PG_PSW'),
+  //       // entities: [
+  //       //   UserEntity,
+  //       //   RoleEntity,
+  //       //   UserRolesEntity,
+  //       //   FileEntity,
+  //       //   TrackEntity,
+  //       //   AlbumEntity,
+  //       //   ReactionEntity,
+  //       // ],
+  //       // ^^ ТОЛЬКО DEV true
+  //       // синхрон.табл.БД, логи сборки
+  //       // ! при true начала падать в ошб. - ERROR [TypeOrmModule] Unable to connect to the database. Retrying (1)... QueryFailedError: максимальное число столбцов в таблице: 1600
+  //       synchronize: false,
+  //       logging: false,
+  //     }),
+  //     inject: [ConfigService],
+  //   }),
+  //   // обслуж.статич.контент по путь/папка ч/з веб-сайт
+  //   // ServeStaticModule.forRoot({
+  //   //   rootPath: path.resolve(__dirname, /* '..', */ 'static'),
+  //   //   serveRoot: '/static',
+  //   // }),
+  //   // подкл.использ.modulи
+  //   // // AuthModule,
+  //   // UsersModule,
+  //   // RolesModule,
+  //   // FilesModule,
+  //   // TrackModule,
+  //   // AlbumModule,
+  //   // ReactionsModule,
+  // ],
   // ^^ подкл.App.cnrtl,serv до декомпозиции (для кажд.сущности свой cntrl,serv )
   // подкл.cnrtl данного модуля
   controllers: [AppController, AppController2],
