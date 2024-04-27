1dc8b81b17a671e3aa54ba9fdaa2fc11f0be1450
EvGen Gal <genwolk1@gmail.com>
2024-04-28 02:48:16 +0300

    SRV. нач.config(+localhostConfig)

---
+++ server/src/config/envs/localhost.config.ts
@@ -0,0 +1,34 @@
+export const localhostConfig = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'test',
+  password: 'test',
+  database: 'test',
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  synchronize: true,
+};
+
+export const config = {
+  db: {
+    // entities: [`${__dirname}/../../entity/**/*.{js,ts}`],
+    // subscribers: [`${__dirname}/../../subscriber/**/*.{js,ts}`],
+    // migrations: [`${__dirname}/../../migration/**/*.{js,ts}`],
+  },
+  graphql: {
+    debug: true,
+    playground: {
+      settings: {
+        'request.credentials': 'include',
+      },
+    },
+    autoSchemaFile: true,
+    autoTransformHttpErrors: true,
+    // cors: { credentials: true },
+    // sortSchema: true,
+    // installSubscriptionHandlers: true,
+  },
+  hello: 'world',
+  jwtSecret: process.env.JWT_SECRET,
+  jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
+};
