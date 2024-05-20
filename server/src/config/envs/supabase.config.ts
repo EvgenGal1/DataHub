6e53bc00f7f2c847bd412335b862557f67f6b6a9
EvGen Gal <genwolk1@gmail.com>
2024-05-20 23:50:25 +0300

    SRV. мини правки конфиг.ф.

--- server/src/config/envs/supabase.config.ts
+++ server/src/config/envs/supabase.config.ts
@@ -1,12 +1,30 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { AlbumEntity } from 'src/albums/entities/album.entity';
+import { FileEntity } from 'src/files/entities/file.entity';
+import { ReactionEntity } from 'src/reactions/entities/reaction.entity';
+import { RoleEntity } from 'src/roles/entities/role.entity';
+import { UserRolesEntity } from 'src/roles/entities/user-roles.entity';
+import { TrackEntity } from 'src/tracks/entities/track.entity';
+import { UserEntity } from 'src/users/entities/user.entity';
 
 export default (): TypeOrmModuleOptions => ({
   type: 'postgres',
   synchronize: process.env.NODE_ENV !== 'production',
-  entities: ['src/**/entities/*.entity{.ts}'],
-  host: process.env.SUPABASE_PG_HOST,
-  port: /* + */ Number(process.env.SUPABASE_PG_PORT),
-  database: process.env.SUPABASE_PG_DBN,
-  username: process.env.SUPABASE_PG_USER,
-  password: process.env.SUPABASE_PG_PSW,
+  entities: [
+    UserEntity,
+    RoleEntity,
+    UserRolesEntity,
+    FileEntity,
+    TrackEntity,
+    AlbumEntity,
+    ReactionEntity,
+    // ! не отраб.подкл.по пути - постояный сбор query: SELECT * FROM current_schema() | ошб.в swg - "statusCode": 500, "message": "Internal server error
+    // `../../*/entities/*.entity.ts`, `src/*/entities/*.entity{.ts,.js}`, `src/**/*.entity.ts`, `*/entities/**.entity{.ts}`, path.join(__dirname, 'src', '**', 'entities', '*.entity.{ts,js}'), 'src/**/entities/*.entity{.ts}'
+  ],
+  url: process.env.SB_PG_URL,
+  host: process.env.SB_PG_HOST,
+  port: /* + */ Number(process.env.SB_PG_PORT),
+  database: process.env.SB_PG_DBN,
+  username: process.env.SB_PG_USER,
+  password: process.env.SB_PG_PSW,
 });
