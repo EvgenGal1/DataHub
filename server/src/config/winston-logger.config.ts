83e46e10dd7d066eeb31c63a262eda76c8013748
EvGen Gal <genwolk1@gmail.com>
2024-05-28 01:35:33 +0300

    SRV. правки W-logger > запуск vercel

--- server/src/config/winston-logger.config.ts
+++ server/src/config/winston-logger.config.ts
@@ -1,7 +1,7 @@
 // ^ logger на winston
 import * as winston from 'winston';
 import 'winston-daily-rotate-file';
-import * as path from 'path';
+// import * as path from 'path';
 
 // константы > команды запуска process.env.NODE_ENV
 import { isProduction } from './envs/env.consts';
@@ -19,19 +19,20 @@ export const WinstonLoggerProvider = {
       ),
       transports: [
         new winston.transports.Console(),
-        new winston.transports.DailyRotateFile({
-          filename: 'application-%DATE%.log',
-          datePattern: 'YYYY-MM-DD',
-          zippedArchive: true,
-          maxSize: '20m',
-          maxFiles: '14d',
-          // dirname: path.join(
-          //   process.cwd(),
-          //   isProduction ? `'/tmp' 'logs'` : 'logs',
-          // ),
-          dirname: path.join(process.cwd(), '/tmp', 'logs'),
-          // dirname: isProduction ? path.join('tmp', 'logs') : 'logs',
-        }),
+        // ! ошб.врем.откл. - ошб.vercel - read-only file system, mkdir '/var/task/' | '/var/logs/'
+        // new winston.transports.DailyRotateFile({
+        //   filename: 'application-%DATE%.log',
+        //   datePattern: 'YYYY-MM-DD',
+        //   zippedArchive: true,
+        //   maxSize: '20m',
+        //   maxFiles: '14d',
+        //   // dirname: path.join(
+        //   //   process.cwd(),
+        //   //   isProduction ? `'/tmp' 'logs'` : 'logs',
+        //   // ),
+        //   dirname: path.join(process.cwd(), '/tmp', 'logs'),
+        //   // dirname: isProduction ? path.join('tmp', 'logs') : 'logs',
+        // }),
       ],
     });
   },
