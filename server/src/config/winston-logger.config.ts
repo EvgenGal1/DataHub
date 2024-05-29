320c8cba8af6035c03c297e3f5aba40e003de333
EvGen Gal <genwolk1@gmail.com>
2024-05-29 18:02:24 +0300

    SRV. правки winston > log vercel

--- server/src/config/winston-logger.config.ts
+++ server/src/config/winston-logger.config.ts
@@ -1,29 +1,50 @@
 // ^ logger на winston
 // import * as winston from 'winston';
-import { createLogger, transports, format } from 'winston';
+import { createLogger, transports, format, transport } from 'winston';
 import 'winston-daily-rotate-file';
 import * as path from 'path';
 
 // константы > команды запуска process.env.NODE_ENV, winston
-import { isProduction } from './envs/env.consts';
+import { /* isProduction, */ isDevelopment, isTotal } from './envs/env.consts';
 const { timestamp, combine, json, errors } = format;
 
 export const WinstonLoggerProvider = {
   provide: 'WINSTON_LOGGER',
   useFactory: () => {
-    return createLogger({
-      // level: 'info',
-      // levles: npm.levels,
-      level: isProduction ? 'info' : 'debug',
-      format: combine(errors({ stack: true }), timestamp(), json()),
-      transports: [
-        new transports.Console(),
-        new transports.File({
-          filename: 'app222-%DATE%.log',
+    // перем.transport > нес.кмд.запуска
+    const transportsArray: transport[] = [new transports.Console()];
+    // опред.мкд.запуск
+    if (isDevelopment || isTotal) {
+      transportsArray.push(
+        // new FileRotateTransport // настр. > Elasticsearch + Kibana
+        // ! ошб.врем.откл. - ошб.vercel - read-only file system, mkdir '/var/task/' | '/var/logs/'
+        // дател.гастр.ф. > кажд.день
+        new transports.DailyRotateFile({
+          filename: 'app(DailyRotate)-%DATE%.log',
+          datePattern: 'YYYY-MM-DD',
           zippedArchive: true,
-          dirname: path.join(process.cwd(), 'src/tmp/logs'),
+          maxSize: '20m',
+          maxFiles: '7d',
+          dirname: path.join(
+            // __dirname,
+            process.cwd(),
+            // isProduction ? './src/tmp/logs' : './src/tmp/logs',
+            '/tmp/logs',
+          ),
         }),
-      ],
+        new transports.File({
+          filename: 'app(File)-%DATE%.log',
+          zippedArchive: true,
+          dirname: path.join(process.cwd(), '/tmp/logs'),
+        }),
+      );
+    }
+
+    return createLogger({
+      // levels: config.npm.levels,
+      level: isDevelopment ? 'debug' : 'info',
+      format: combine(errors({ stack: true }), timestamp(), json()),
+      transports: transportsArray,
     });
   },
 };
