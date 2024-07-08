6196aab49eeb6dcb4d8b0228cbe3239c23bdb5ce
EvGen Gal <ZBst.1@yandex.ru>
2024-07-09 02:00:40 +0300

    CLT. #stl, #NotFound

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,46 +1,65 @@
 export default function NotFound() {
+  // локал.инлайн css
   const styles = {
-    err: {
+    notf: {
+      width: "50%",
+      margin: "0 auto",
       display: "flex",
       flexDirection: "column" as const,
       alignItems: "center",
       justifyContent: "center",
     },
-    errTitle: {
+    notfTitle: {
       fontSize: "20px",
       fontWeight: "bold",
     },
-    h1: {
-      margin: "0 25px 0 0",
-      padding: "0 25px 0 0",
-      borderRight: "1px solid rgba(0, 0, 0, 0.3)",
-    },
-    errMessage: {},
-    errContext: {
-      padding: "0",
-    },
-    errControl: { width: "50%" },
-    errControlLink: {
-      padding: "5px 10px",
-      borderRadius: "10px",
-      textDecoration: "underline",
-      fontWeight: "bold",
-    },
+    // notfMessage: {},
+    // notfContext: { textAlign: "center" as const },
+    // notfControl: {},
+    // notfControlLink: { fontWeight: "bold" },
   };
 
   return (
     <>
-      <div className="main-screen dfrcc err" style={styles.err}>
+      {/* локал.use-clt стили CSS-in-JS в NextJS на `style jsx` */}
+      {/* <style jsx>{`
+        .main-screen {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+        }
+        .notf__title {
+          display: inline-block;
+          margin: 0 20px 0 0;
+          padding: 0 23px 0 0;
+        }
+        .notf__message {
+          display: inline-block;
+        }
+        @media (prefers-color-scheme: dark) {
+          .notf__title {
+            border-right: 1px solid rgba(255, 255, 255, 0.3);
+          }
+        }
+      `}</style> */}
+      <div className="main-screen dfrcc notf" style={styles.notf}>
         {/* авто переадрес на Главную ч/з 30 сек. */}
         <head>
-          <meta http-equiv="refresh" content="30;url=/" />
+          <meta http-equiv="refresh" content="9930;url=/" />
         </head>
-        <div className="err__title dfrcc" style={styles.errTitle}>
-          <h1 style={styles.h1}>404</h1>
+        <div className="notf__title dfrcc" style={styles.notfTitle}>
+          <h1 className="mr-5 pr-5">404</h1>
           <div>Страница не найдена</div>
         </div>
-        <div className="err__context df df-dc mt-3" style={styles.errContext}>
-          <p className="err__message error__message--important">
+        <div
+          className="notf__context w-100 df df-dc mt-3"
+          // style={styles.notfContext}
+        >
+          <p
+            className="notf__message"
+            // style={styles.notfMessage}
+          >
             Проверьте URL, используйте окно поиска или ссылки ниже
           </p>
           <input
@@ -50,19 +69,21 @@ export default function NotFound() {
           />
         </div>
         <div
-          className="err__control df df-aic df-jcse mt-5"
-          style={styles.errControl}
+          className="notf__control w-100 df df-aic df-jcsb mt-5"
+          // style={styles.notfControl}
         >
-          <a
-            href="javascript:history.back()"
-            className="danger mr-3"
-            style={styles.errControlLink}
-          >
-            Вернуться Назад
-          </a>
-          <a href="/" className="success ml-3" style={styles.errControlLink}>
-            Перейти на Главную
-          </a>
+          <div>
+            <span></span>
+            <a href="javascript:history.back()" className="danger">
+              <span>Вернуться </span>Назад
+            </a>
+          </div>
+          <div>
+            <span></span>
+            <a href="/" className="success">
+              Главная <span>страница</span>
+            </a>
+          </div>
         </div>
       </div>
     </>
