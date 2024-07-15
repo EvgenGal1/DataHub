c4e3c140d6a8aa0a353ce307e510f1aeac9df9c3
EvGen Gal <ZBst.1@yandex.ru>
2024-07-16 00:55:21 +0300

    CLT. +Комп.Стрелки

---
+++ client/src/components/icons/ChevronLeftIcon.tsx
@@ -0,0 +1,13 @@
+export default function ChevronLeftIcon() {
+  return (
+    <svg
+      className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root"
+      focusable="false"
+      aria-hidden="true"
+      viewBox="0 0 24 24"
+      data-testid="ChevronLeftIcon"
+    >
+      <path d="M15.41 7.41 14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path>
+    </svg>
+  );
+}
