e425622a9b439a3168ad20c18253b8dae39a9215
EvGen Gal <ZBst.1@yandex.ru>
2024-07-17 23:37:05 +0300

    CLT. #stl, #NavBar > продолж.очистки от MUI

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -10,11 +10,6 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
-import AudiotrackIcon from "@mui/icons-material/Audiotrack";
-import AlbumIcon from "@mui/icons-material/Album";
-import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
-import InboxIcon from "@mui/icons-material/Inbox";
 import MailIcon from "@mui/icons-material/Mail";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import PersonIcon from "@mui/icons-material/Person";
@@ -26,6 +21,11 @@ import PersonIcon from "@mui/icons-material/Person";
 import MenuIcon from "./icons/MenuIcon";
 import ChevronLeftIcon from "./icons/ChevronLeftIcon";
 import ChevronRightIcon from "./icons/ChevronRightIcon";
+import InboxIcon from "./icons/InboxIcon";
+import CloudDownloadIcon from "./icons/CloudDownloadIcon";
+import AlbumIcon from "./icons/AlbumIcon";
+import AudiotrackIcon from "./icons/AudiotrackIcon";
+import PlaylistPlayIcon from "./icons/PlaylistPlayIcon";
 
 // ^ БОКОВАЯ ПАНЕЛЬ | SIDE BAR
 // масс.верх.эл/путей бок.панель
