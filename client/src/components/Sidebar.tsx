03069131915d5680605a1f5230d238242ec455f5
EvGen Gal <ZBst.1@yandex.ru>
2024-08-08 02:54:48 +0300

    CLT. Sidebar (нов.ul, распред.пунктов, stl)

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -5,27 +5,37 @@ import { usePathname, useRouter } from "next/navigation";
 
 import ChevronLeftIcon from "./icons/ChevronLeftIcon";
 import ChevronRightIcon from "./icons/ChevronRightIcon";
-import AlbumIcon from "./icons/AlbumIcon";
+import File from "./icons/File";
 import AudiotrackIcon from "./icons/AudiotrackIcon";
-import CloudDownloadIcon from "./icons/CloudDownloadIcon";
-import InboxIcon from "./icons/InboxIcon";
+import Book from "./icons/Book";
+import Image from "./icons/Image";
+import Video from "./icons/Video";
 import PlaylistPlayIcon from "./icons/PlaylistPlayIcon";
+import AlbumIcon from "./icons/AlbumIcon";
+import CloudDownloadIcon from "./icons/CloudDownloadIcon";
 import MailIcon from "./icons/MailIcon";
 import ShoppingCartIcon from "./icons/ShoppingCartIcon";
 import PersonIcon from "./icons/PersonIcon";
+// import InboxIcon from "./icons/InboxIcon";
 
-// названия/пути/эл. пунктов боковой панели верх/низ
+// названия/пути/эл. пунктов боковой панели верх/середина/низ
 const sideBarTopItems = [
-  { text: "Главная", href: "/", elm: <InboxIcon /> },
-  { text: "Закачать", href: "/download", elm: <CloudDownloadIcon /> },
+  { text: "Файлы", href: "/files", elm: <File /> },
   { text: "Треки", href: "/tracks", elm: <AudiotrackIcon /> },
-  { text: "Альбомы", href: "/albums", elm: <AlbumIcon /> },
+  { text: "Книги", href: "/books", elm: <Book /> },
+  { text: "Картинки", href: "/images", elm: <Image /> },
+  { text: "Видео", href: "/videos", elm: <Video /> },
+];
+const sideBarMiddleItems = [
   { text: "Плейлисты", href: "/playlists", elm: <PlaylistPlayIcon /> },
+  { text: "Альбомы", href: "/albums", elm: <AlbumIcon /> },
+  { text: "Закачать", href: "/download", elm: <CloudDownloadIcon /> },
 ];
 const sideBarBottomItems = [
   { text: "Почта", href: "/mail", elm: <MailIcon /> },
   { text: "Корзина", href: "/basket", elm: <ShoppingCartIcon /> },
-  { text: "Личный Кабинет", href: "/personal", elm: <PersonIcon /> },
+  { text: "ЛК", href: "/personal", elm: <PersonIcon /> },
+  // { text: "Главная", href: "/", elm: <InboxIcon /> },
 ];
 
 const Sidebar: React.FC<{ isOpen: boolean; handleDrawerClose: () => void }> = ({
@@ -66,6 +76,26 @@ const Sidebar: React.FC<{ isOpen: boolean; handleDrawerClose: () => void }> = ({
           ))}
         </ul>
         <hr className="hr hr-big" />
+        <ul>
+          {sideBarMiddleItems.map(({ text, href, elm }) => (
+            <li
+              key={href}
+              onClick={() => router.push(href)}
+              className={`link ${pathname === href ? "active" : ""}`}
+            >
+              <div
+                className="hover-el"
+                style={{ justifyContent: isOpen ? "initial" : "center" }}
+              >
+                <div style={{ marginRight: isOpen ? 24 : "auto" }}>{elm}</div>
+                <div style={{ opacity: isOpen ? 1 : 0 }}>
+                  <span>{text}</span>
+                </div>
+                <span className="hz"></span>
+              </div>
+            </li>
+          ))}
+        </ul>
         <ul>
           {sideBarBottomItems.map(({ text, href, elm }) => (
             <li
