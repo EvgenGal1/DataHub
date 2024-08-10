24555cfcf4f6083885aa0d1f6d7b93029bedf280
EvGen Gal <ZBst.1@yandex.ru>
2024-08-10 14:46:43 +0300

    CLT. Post (разделение Blog <> Posts/Search )

--- client/src/app/(marketing)/blog/page.tsx
+++ client/src/app/(marketing)/blog/page.tsx
@@ -1,41 +1,32 @@
-import { Metadata } from "next";
-import Link from "next/link";
+"use client";
 
-// вспомог.fn получ.данн. Отраб.на serv.
-async function getDate() {
-  const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts",
-    // расшир.доп.объ.настр.fetch от Next > повотр.провер.данн.кеша (здесь раз в час проверка)
-    { next: { revalidate: 3600 } }
-  );
-  // ошб.е/и не ok
-  if (!response.ok) {
-    throw new Error(
-      `ok: ${response?.ok}\nstatus: ${response?.status}\nsms: ${response?.statusText}\nurl: ${response?.url}`
-    );
-  }
-  return response.json();
-}
+import { useEffect, useState } from "react";
 
-// локал.объ.настр.метадаты > SEO
-export const metadata: Metadata = {
-  title: "Data Hub | Blog",
-};
+import Loading from "@/app/loading";
+import { Posts } from "@/components/Posts";
+import { PostSearch } from "@/components/PostSearch";
+import { getAllPosts } from "@/services/getPosts";
+import { Post } from "@/types/Post";
 
 // serv.Комп могут быть async
-export default async function Blog() {
+export default function Blog() {
+  // сост.постов,загр.,ошб.
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
   // асинхр.загр.данн.
-  const posts = await getDate();
+  useEffect(() => {
+    getAllPosts()
+      .then(setPosts)
+      .catch((error) => console.error("Ошибка при загрузке постов:", error))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <div className="blog">
-      <h1>Blog page Blog</h1>
-      <ul>
-        {posts.map((post: any) => (
-          <li key={post.id}>
-            <Link href={`/blog/${post.id}`}>{post.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <h1>Cтраница Блога</h1>
+      <PostSearch onSearch={setPosts} />
+      {loading ? <Loading /> : <Posts posts={posts} />}
     </div>
   );
 }
