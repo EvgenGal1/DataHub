c68aaa55b3dc2e674a700015f2f24d11f285bb57
EvGen Gal <ZBst.1@yandex.ru>
2024-09-17 02:30:10 +0300

    CLT. +api/auth/[...nextauth]/route (api динам.роуты аутентиф.)

---
+++ client/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,11 @@
+// api динам.роуты аутентиф.
+
+import NextAuth from "next-auth";
+// подкл.конфиг.auth
+import { authConfig } from "@/configs/auth/auth";
+
+// обработка fn NextAuth по созд.конфиг.
+const handler = NextAuth(authConfig);
+
+// возвращ.переимен.мтд.
+export { handler as GET, handler as POST };
