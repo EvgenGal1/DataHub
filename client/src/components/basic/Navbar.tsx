e7936cf32dff5f1b9118f930dcf9c9131b54c73e
EvGen Gal <ZBst.1@yandex.ru>
2024-09-20 01:34:38 +0300

    CLT. #NavBar(+useSession, +кнп.вход/выход/профиль от session)

--- client/src/components/basic/Navbar.tsx
+++ client/src/components/basic/Navbar.tsx
@@ -1,7 +1,10 @@
 // ссылки шапки
 
+"use client";
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useSession, /* signIn, */ signOut } from "next-auth/react";
 
 // названия/пути/эл. пунктов шапки
 const navItems = [
@@ -13,10 +16,14 @@ const navItems = [
 
 // const Navbar: React.FC = () => {
 export default function Navbar() {
+  // получ.текущее место URL
   const pathname = usePathname();
+  // получ.зарегистр.данн.польз.сессии ! use clt
+  const session = useSession();
 
   return (
     <nav className="nav flex ml-auto">
+      {/* базовые ссылки */}
       {navItems.map(({ text, href }) => (
         <Link
           key={href}
@@ -31,6 +38,28 @@ export default function Navbar() {
           <span>{text}</span>
         </Link>
       ))}
+      {/* Профиль при сессии */}
+      {session?.data && (
+        <Link href="/profile" className="link">
+          Profile
+        </Link>
+      )}
+      {/* вход/выход при сессии */}
+      {session?.data ? (
+        // выход с редирект на гл.по клиик
+        <Link
+          href="#"
+          onClick={() => signOut({ callbackUrl: "/" })}
+          className="link"
+        >
+          Sign Out
+        </Link>
+      ) : (
+        // вход по стандарт.URL
+        <Link href="/api/auth/signin" className="link">
+          Sign In
+        </Link>
+      )}
     </nav>
   );
 }
