d983d616707e8925ba0b793ee5f02a2b87930c7e
EvGen Gal <ZBst.1@yandex.ru>
2024-09-22 20:23:39 +0300

    CLT. +profile/page(ф.профиля с данн.сессии getServerSession), #next.config{images{domains:["..."]}(указ.допустим.хостинг > Image)

---
+++ client/src/app/profile/page.tsx
@@ -0,0 +1,30 @@
+// стр.Профиля
+
+import { getServerSession } from "next-auth/next";
+import Image from "next/image";
+
+import { authConfig } from "@/configs/auth/auth";
+
+// async srv.Комп.
+export default async function Profile() {
+  // получ.данн.сессии
+  const session = await getServerSession(authConfig);
+
+  return (
+    <div>
+      {/* отрис.name и по услов.image */}
+      <h1>Profile of {session?.user?.name}</h1>
+      {session?.user?.image && (
+        <Image
+          src={session.user.image}
+          alt=""
+          width={100}
+          height={100}
+          objectFit="contain"
+          layout="intrinsic"
+          style={{ maxWidth: "100%", width: "auto", height: "auto" }}
+        />
+      )}
+    </div>
+  );
+}
