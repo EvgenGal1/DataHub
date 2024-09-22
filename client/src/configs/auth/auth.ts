458696e009347c0230d03b86d0b4e5cccdd5b4c2
EvGen Gal <ZBst.1@yandex.ru>
2024-09-22 22:00:49 +0300

    CLT. #config/auth(вход eml/psw ч/з Credentials и данн.фейк users), + данн.фейк users

--- client/src/configs/auth/auth.ts
+++ client/src/configs/auth/auth.ts
@@ -1,15 +1,49 @@
 // конфиг.авторизации/аутентификации
 
-import type { AuthOptions } from "next-auth";
+import type { AuthOptions, User } from "next-auth";
 import GoggleProvider from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+
+// фейк данн.Пользователей
+import { usersFake } from "@/data/usersFake";
 
 export const authConfig: AuthOptions = {
   // провайдеры(обязат.парам.)
   providers: [
-    // провайдер гугл.вызова fn с настр.ID и SECRET ч/з Google Cloud Console OAuth APLs&Services
+    // fn гугл.вход
     GoggleProvider({
+      // настр.ID и SECRET ч/з Google Cloud Console OAuth APLs&Services
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+    // fn вход по логин/пароль
+    Credentials({
+      // поля eml/psw с указ.:обводки,типа,обязат.
+      credentials: {
+        email: { label: "email", type: "email", required: true },
+        password: { label: "password", type: "password", required: true },
+      },
+      // async мтд.объ.с приёмом реквизитов входа
+      async authorize(credentials) {
+        // проверки отсутствия eml/psw
+        if (!credentials?.email || !credentials.password) return null;
+
+        // ^^ req/res получ.данн.с БД
+
+        // фейк данн.польз.(только > next). перебор
+        const currentUser = usersFake.find(
+          (userFake) => userFake.email === credentials.email
+        );
+        // фейк данн.польз. соответ.ведённым данн.
+        if (currentUser && currentUser.password === credentials.password) {
+          const { password, ...userWithoutPass } = currentUser;
+          // возвращ.user под опред.типом без psw
+          console.log("userWithoutPass : ", userWithoutPass);
+          return userWithoutPass as User;
+        }
+        // null е/и не авториз.
+        return null;
+      },
     }),
   ],
 };
---
+++ client/src/data/usersFake.ts
@@ -0,0 +1,25 @@
+// данн.фейк Пользователей / имитация БД
+
+export const usersFake = [
+  {
+    id: "1",
+    email: "eg@ge.com",
+    name: "EvGen",
+    password: "12345",
+    role: "admin",
+  },
+  {
+    id: "2",
+    email: "admin@ad.ru",
+    name: "Super Admin",
+    password: "12345",
+    role: "admin",
+  },
+  {
+    id: "3",
+    email: "any@an.com",
+    name: "Гость",
+    password: "12345",
+    role: "guest",
+  },
+];
