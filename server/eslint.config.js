c4d50f3df13ea7f61e8253b1634063fcff097cec
EvGen Gal <ZBst.1@yandex.ru>
2024-10-06 23:36:28 +0300

    SRV. замена eslint на соврем. config

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,4 +1,7 @@
-module.exports = {
+// eslint.config.js
+import { defineConfig } from 'eslint-define-config';
+
+export default defineConfig({
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
@@ -7,6 +10,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
@@ -21,7 +25,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    // от ошб. Delete `␍`eslint
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'no-extra-parens': 'off',
   },
-};
+});
