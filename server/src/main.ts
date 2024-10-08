780379076fd5f65179f9cce6bf4fe5d0be94057c
EvGen Gal <ZBst.1@yandex.ru>
2024-10-08 19:30:46 +0300

    SRV. правки ошб.сборки VERCEL #main -cg

--- server/src/main.ts
+++ server/src/main.ts
@@ -90,7 +90,8 @@ async function bootstrap(): Promise<any> {
 
     let url: string;
     // прослуш.PORT и fn()callback с cg на Запуск
-    /* return */ await app.listen(PORT, () => {
+    /* return */ await app.listen(
+      PORT /* , () => {
       // console.log(`Запуск Сервер > PORT ${PORT}`);
       // ^ вывод подкл.к БД от NODE_ENV. производство(БД SB) <> разработка (dev БД SB, total БД SB, LH)
       let srt: string, port: string, source: string;
@@ -112,7 +113,8 @@ async function bootstrap(): Promise<any> {
         url = process.env.VERCEL_URL;
       }
       console.log(`${srt}. Сервер - ${port}, подключён '${source}' - ${url}`);
-    });
+    } */,
+    );
     // if (isDevelopment)logger.info(`Приложение работает на: ${/* await app.getUrl() */ url}`);
   } catch (e) {
     console.log('main e : ' + e);
