782bccb65e9cb57ce6c492b3a5c830aa08e0ad96
EvGen Gal <ZBst.1@yandex.ru>
2024-10-08 19:24:49 +0300

    SRV. правки ошб.сборки VERCEL #main +logger

--- server/src/main.ts
+++ server/src/main.ts
@@ -23,21 +23,21 @@ async function bootstrap(): Promise<any> {
     });
 
     // логи
-    // const logger = app.get('WINSTON_LOGGER');
-    // app.useLogger(logger);
-    // if (isDevelopment || isTotal) {
-    //   // созд.п. > логи
-    //   const tmpDir = path.join(process.cwd(), 'tmp');
-    //   if (!fs.existsSync(tmpDir)) {
-    //     console.log('123 : ' + 123);
-    //     fs.mkdirSync(tmpDir, { recursive: true });
-    //   }
-    //   const logDir = path.join(tmpDir, 'logs');
-    //   if (!fs.existsSync(logDir)) {
-    //     console.log('345 : ' + 345);
-    //     fs.mkdirSync(logDir, { recursive: true });
-    //   }
-    // }
+    const logger = app.get('WINSTON_LOGGER');
+    app.useLogger(logger);
+    if (isDevelopment || isTotal) {
+      // созд.п. > логи
+      const tmpDir = path.join(process.cwd(), 'tmp');
+      if (!fs.existsSync(tmpDir)) {
+        console.log('123 : ' + 123);
+        fs.mkdirSync(tmpDir, { recursive: true });
+      }
+      const logDir = path.join(tmpDir, 'logs');
+      if (!fs.existsSync(logDir)) {
+        console.log('345 : ' + 345);
+        fs.mkdirSync(logDir, { recursive: true });
+      }
+    }
 
     // обраб.ошб.ч/з глобал.обраб.исключений
     app.useGlobalFilters(new HttpExceptionFilter());
@@ -90,8 +90,7 @@ async function bootstrap(): Promise<any> {
 
     let url: string;
     // прослуш.PORT и fn()callback с cg на Запуск
-    /* return */ await app.listen(
-      PORT /* , () => {
+    /* return */ await app.listen(PORT, () => {
       // console.log(`Запуск Сервер > PORT ${PORT}`);
       // ^ вывод подкл.к БД от NODE_ENV. производство(БД SB) <> разработка (dev БД SB, total БД SB, LH)
       let srt: string, port: string, source: string;
@@ -113,9 +112,9 @@ async function bootstrap(): Promise<any> {
         url = process.env.VERCEL_URL;
       }
       console.log(`${srt}. Сервер - ${port}, подключён '${source}' - ${url}`);
-    } */,
-    );
-    // if (isDevelopment) logger.info(`Приложение работает на: ${/* await app.getUrl() */ url}`);
+    });
+    if (isDevelopment)
+      logger.info(`Приложение работает на: ${/* await app.getUrl() */ url}`);
   } catch (e) {
     console.log('main e : ' + e);
   }
