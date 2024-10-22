f2d05b36a7aa28ca1cd5ca53df306395e38cd122
EvGen Gal <ZBst.1@yandex.ru>
2024-10-22 03:44:47 +0300

    SRV. # app.cntrl > ! Vercel

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -24,8 +24,8 @@ export class AppController {
     // после внедр.завис. обращ.к serv ч/з this
     // return this.appService.getHello();
     // возвращ.ф.HTML по пути из раб.п.
-    res.sendFile('/public/views/pages/welcome.html', { root: __dirname });
-    // res.sendFile('/views/pages/welcome.html', { root: 'public' });
+    // res.sendFile('/public/views/pages/welcome.html', { root: __dirname });
+    res.sendFile('/public/views/pages/welcome.html', { root: 'public' });
   }
 
   @Get('/details')
