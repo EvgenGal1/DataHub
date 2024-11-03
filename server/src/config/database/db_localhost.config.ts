8115f660c079c63d798301025abe9597e84ba472
EvGen Gal <ZBst.1@yandex.ru>
2024-11-04 01:58:44 +0300

    SRV. ! DBLocalhostConfig (synchronize: true >> Unable connect database(localhost) >> макс.стлб.1600) ~ synchronize: false

--- server/src/config/database/db_localhost.config.ts
+++ server/src/config/database/db_localhost.config.ts
@@ -27,6 +27,7 @@ export const DBLocalhostConfig = (): TypeOrmModuleOptions => ({
     // ! не отраб.подкл.по пути - постояный сбор query: SELECT * FROM current_schema() | ошб.в swg - "statusCode": 500, "message": "Internal server error
     // `../../*/entities/*.entity.ts`, `src/*/entities/*.entity{.ts,.js}`, `src/**/*.entity.ts`, `*/entities/**.entity{.ts}`, path.join(__dirname, 'src', '**', 'entities', '*.entity.{ts,js}'),
   ],
-  synchronize: true,
-  // logging: true,
+  // synchronize: true, // ! Unable to connect to the database (localhost). Retrying (1)...   >>  QueryFailedError: максимальное число столбцов в таблице: 1600
+  synchronize: false,
+  logging: true,
 });
