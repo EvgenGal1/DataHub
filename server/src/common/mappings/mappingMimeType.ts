8892a18cbf205e03606635bce220338f9619b68c
EvGen Gal <ZBst.1@yandex.ru>
2024-11-06 02:59:13 +0300

    SRV. # f-types (один enum > типы/пути), mapping

--- server/src/common/mappings/mappingMimeType.ts
+++ server/src/common/mappings/mappingMimeType.ts
@@ -1,19 +1,35 @@
+// ^ mapping (расшир.связь ф.mimeType с FileType)
+
 import { FileType } from '../types/file-types';
 
 export const mappingMimeType = {
   audio: FileType.AUDIO,
   image: FileType.IMAGE,
-  text: FileType.TEXT,
-  application: {
-    pdf: FileType.DOCUMENT,
-    doc: FileType.DOCUMENT,
-    docx: FileType.DOCUMENT,
-    zip: FileType.FILE,
-    xlsx: FileType.FILE,
-    pptx: FileType.FILE,
-    'vnd.openxmlformats-officedocument.wordprocessingml.document':
-      FileType.DOCUMENT,
-    msword: FileType.DOCUMENT,
+  text: {
+    plain: FileType.DOCUMENT, // txt
+    html: FileType.WEB,
+    css: FileType.WEB,
+    scss: FileType.WEB,
+    javascript: FileType.WEB,
+    'x-python': FileType.WEB,
+    xml: FileType.DOCUMENT,
+    csv: FileType.IMAGE,
   },
-  video: FileType.FILE,
+  application: {
+    msword: FileType.DOCUMENT, //  стар.DOC
+    xml: FileType.DOCUMENT,
+    json: FileType.DOCUMENT,
+    'schema+json': FileType.DOCUMENT,
+    pdf: FileType.DOCUMENT,
+    'vnd.openxmlformats-officedocument.wordprocessingml.document':
+      FileType.DOCUMENT, // DOCX
+    'vnd.openxmlformats-officedocument.spreadsheetml.sheet': FileType.DOCUMENT, // Excel
+    'vnd.openxmlformats-officedocument.presentationml.presentation':
+      FileType.DOCUMENT, // PowrPoint
+    'octet-stream': FileType.BOOK, // fb2
+    zip: FileType.APPLICATION,
+    'x-rar-compressed': FileType.APPLICATION,
+    'x-msdownload': FileType.APPLICATION,
+  },
+  video: FileType.VIDEO,
 };
