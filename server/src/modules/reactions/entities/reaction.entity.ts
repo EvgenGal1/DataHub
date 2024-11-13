c5d9bc22a0ce967f1f77d5b7f5847854fa1ac3d3
EvGen Gal <ZBst.1@yandex.ru>
2024-11-13 16:28:12 +0300

    SRV. # Entity Reaction (swg, столбцы, связи)

--- server/src/modules/reactions/entities/reaction.entity.ts
+++ server/src/modules/reactions/entities/reaction.entity.ts
@@ -1,24 +1,70 @@
-import { Entity, Column, ManyToOne, PrimaryColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  ManyToOne,
+  PrimaryColumn,
+  ManyToMany,
+  CreateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 import { UserEntity } from '../../users/entities/user.entity';
-import { TrackEntity } from '../../tracks/entities/track.entity';
 
 @Entity({ name: 'reactions', schema: 'public' })
 export class ReactionEntity {
-  // id, текст реакции, id.user, id.track
-  @PrimaryColumn()
+  // id, текст, оценка, id.user, тип объ, id объ
+  @PrimaryColumn({ type: 'integer', unique: true })
+  @ApiProperty({ description: 'Уникальный идентификатор Реакции' })
   id: number;
 
-  @Column({ default: 'реакция #' })
-  text: string;
+  // Поле для текста Реакции
+  @Column({ nullable: true, length: 500 })
+  @ApiProperty({
+    example: 'Мне очень понравился этот трек!',
+    description: 'Текст Реакции',
+  })
+  text?: string;
 
-  // связь табл. Мн.к 1му. У Мн.реакц. Один польз.
+  // Оценка Реакции (например, от 1 до 5)
+  @Column({ nullable: true })
+  @ApiProperty({ example: 5, description: 'Оценка Реакции' })
+  rating?: number;
+
   @ManyToOne(() => UserEntity, (user: UserEntity) => user.reactions)
+  @ApiProperty({
+    type: () => UserEntity,
+    description: 'Пользователь, оставивший Реакцию',
+  })
   user: UserEntity;
 
-  // связь табл. Мн.к 1му. У Мн.реакц. Один трек
-  @ManyToOne(() => TrackEntity, (track: TrackEntity) => track.reactions)
-  track: TrackEntity;
+  // Тип рекации: 'track', 'album', 'file'
+  @Column({ length: 10 })
+  @ApiProperty({
+    example: 'track',
+    description: 'Тип объекта Реакция',
+  })
+  reactionType: 'track' | 'album' | 'file';
+
+  // ID объекта на который оставлена Реакция
+  @Column(/* { type: 'uuid' } */)
+  @ApiProperty({
+    example: 12,
+    description: 'ID объекта Реакции',
+  })
+  reactionId: /* string */ number;
+
+  // Метод для приведения ID к UUID(защит.от коллизии) > reactionId: string и остальных ID через PrimaryColumn
+  // addReaction(postId: number) {
+  //   this.reactionId = uuidv4(); // Генерация нового уникального идентификатора для реакции
+  //   this.postId = postId; // Заполнение стандартным идентификатором поста
+  // }
+
+  @CreateDateColumn({ name: 'createdAt' })
+  startDate?: Date;
+
+  @DeleteDateColumn({ name: 'deletedAt' })
+  deletedAt?: Date;
 
   // ^^ прописать совместную таблицу под лайки, комменты, реакции. Подтягивать только то что нужно к запросу.
   // Например на один трек от 3х user может быть по 3 лайка, коммента, репоста и это займёт только 3 строчки в данн.табл.
