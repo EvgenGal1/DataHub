3096f7c5cf94a686557899a38cd61e47e7c84bf6
EvGen Gal <ZBst.1@yandex.ru>
2024-11-18 01:20:02 +0300

    SRV. # Album str > total_duration

--- server/src/modules/albums/dto/create-album.dto.ts
+++ server/src/modules/albums/dto/create-album.dto.ts
@@ -70,7 +70,7 @@ export class CreateAlbumDto {
   readonly total_tracks?: number;
 
   @ApiProperty({
-    example: '00:00:00',
+    example: '00:30:00',
     description: 'Длительность Треков в Альбоме',
     required: false,
   })
