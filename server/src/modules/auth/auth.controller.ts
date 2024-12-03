4c3b02a9b920728cc83fc67e993b112f6c450356
EvGen Gal <ZBst.1@yandex.ru>
2024-12-03 05:04:07 +0300

    SRV. # Ayth Swagger

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,19 +1,33 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { AuthService } from './auth.service';
 import { LoginAuthDto } from './dto/login-auth.dto';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 
-@Controller('auth')
+@Controller('/auth')
+@ApiTags('Аутентификация')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('register')
+  @Post('/register')
+  @ApiOperation({ summary: 'Регистрация Пользователя' })
+  @ApiResponse({
+    status: 201,
+    description: 'Пользователь успешно зарегистрирован.',
+  })
+  @ApiResponse({ status: 400, description: 'Некорректные данные.' })
   async register(@Body() createUserDto: CreateUserDto) {
     return this.authService.register(createUserDto);
   }
 
-  @Post('login')
+  @Post('/login')
+  @ApiOperation({ summary: 'Аутентификация пользователя' })
+  @ApiResponse({
+    status: 200,
+    description: 'Пользователь успешно авторизован.',
+  })
+  @ApiResponse({ status: 401, description: 'Неверные учетные данные.' })
   async login(@Body() loginDto: LoginAuthDto) {
     return this.authService.login(loginDto);
   }
