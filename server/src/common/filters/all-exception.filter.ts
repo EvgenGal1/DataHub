d8ffc6dcdff5803fa1746684c0102c241f74865a
EvGen Gal <ZBst.1@yandex.ru>
2024-12-27 20:37:34 +0300

    SRV. # AllExceptionsFilter (правки)

--- server/src/common/filters/all-exception.filter.ts
+++ server/src/common/filters/all-exception.filter.ts
@@ -1,4 +1,4 @@
-// ^ глобал.фильтр исключ. > обраб.ошб.HTTP/исключ. и возвращ. статус,время,url,msg
+// ^ глобал.фильтр исключ. > перехвата/обраб.ошб.HTTP/исключ. и возвращ. статус,время,url,мтд,msg
 
 import {
   Catch,
@@ -10,14 +10,15 @@ import {
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 
+import { isDevelopment } from '../../config/envs/env.consts';
+
 // объ.сопоставления КОД - СМС
 const statusMessages: { [key: number]: string } = {
-  [HttpStatus.BAD_REQUEST]: 'Некорректные данные',
-  [HttpStatus.UNAUTHORIZED]: 'Доступ к ресурсу запрещён',
-  [HttpStatus.FORBIDDEN]: 'У вас нет прав для доступа к этому ресурсу',
   [HttpStatus.NOT_FOUND]: 'Ресурс не найден',
-  [HttpStatus.NOT_ACCEPTABLE]:
-    'Запрос не может быть обработан, так как формат данных неприемлем',
+  [HttpStatus.BAD_REQUEST]: 'Ошибка запроса, некорректные данные',
+  [HttpStatus.UNAUTHORIZED]: 'Доступ к ресурсу запрещён',
+  [HttpStatus.FORBIDDEN]: 'У вас нет прав для доступа',
+  [HttpStatus.NOT_ACCEPTABLE]: 'Запрос не обработан, формат данных неприемлем',
   [HttpStatus.CONFLICT]: 'Конфликт данных',
   [HttpStatus.GONE]: 'Запрашиваемый ресурс больше не доступен',
   [HttpStatus.INTERNAL_SERVER_ERROR]: 'Внутренняя ошибка сервера',
@@ -27,54 +28,34 @@ const statusMessages: { [key: number]: string } = {
 export class AllExceptionsFilter implements ExceptionFilter {
   private readonly logger = new Logger(AllExceptionsFilter.name);
 
+  // мтд.обраб.исключ.
   catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
+
+    // состав.статус
     const status =
       exception instanceof HttpException
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
 
-    let message: string;
-    // обраб.общ.ошб.
-    if (exception instanceof HttpException) {
-      const exceptionResponse: any = exception.getResponse();
-      const exceptionMessage = Array.isArray(exceptionResponse?.message)
-        ? exceptionResponse.message.join(', ')
-        : exceptionResponse?.message || null;
-      message =
-        // exceptionMessage || statusMessages[status] || 'Неизвестная ошибка'; // ^ ответ: exception.message <> statusMessages.status <> unknown
-        exceptionMessage // ^ ответ: склейка statusMessages.status с exception.message <> unknown
-          ? `${statusMessages[status] || 'Неизвестная ошибка'}: ${exceptionMessage}`
-          : statusMessages[status] || 'Неизвестная ошибка';
-    }
-    // обраб.ошб.БД: нарушения уникальности <> пустых значений
-    else if (
-      exception instanceof Error &&
-      (exception as any).code === '23505'
-    ) {
-      const errorMessage =
-        (exception as any).detail
-          // удал.скобки
-          ?.replace(/[()]/g, '')
-          // замена двойных на одинарные кавычки
-          .replace(/"([^"]+)"/g, "'$1'") ||
-        'Пользователь уже существует. Замените fullName или email';
-      message = errorMessage;
-    } else {
-      message = statusMessages[status] || 'Неизвестная ошибка';
-    }
+    // Получаем сообщение об ошибке
+    console.log('alEr exception : ', exception);
+    const message = this.getErrorMessage(exception, status);
 
+    // объ.ошб.res
     const errorResponse = {
       statusCode: status,
       timestamp: new Date().toISOString(),
-      path: request.url,
-      method: request.method,
-      message /* : exception instanceof HttpException ? exception.message : message */,
-      error:
-        exception instanceof Error ? exception.message : 'Неизвестная ошибка',
-      // ...(exception instanceof HttpException && exception.getResponse && { error: exception.getResponse() }),
+      path: ctx.getRequest<Request>().url,
+      method: ctx.getRequest<Request>().method,
+      message,
+      error: isDevelopment
+        ? exception instanceof Error
+          ? statusMessages[status]
+          : 'Неизвестная ошибка'
+        : null,
     };
 
     this.logger.error(
@@ -83,5 +64,30 @@ export class AllExceptionsFilter implements ExceptionFilter {
     );
 
     response.status(status).json(errorResponse);
+  }
+
+  // мтд.получ.смс об ошб.
+  private getErrorMessage(exception: unknown, status: number): string {
+    if (exception instanceof HttpException) {
+      const response = exception.getResponse();
+      return typeof response === 'object' && response['message']
+        ? response['message']
+        : statusMessages[status] || 'Неизвестная ошибка';
+    }
+    // общ.ошб.
+    else if (exception instanceof Error) {
+      // специф.ошб.БД
+      if ((exception as any).code === '23505') {
+        return 'Дублирование уникального значения.';
+      }
+      // Возвращаем стандартное сообщение ошибки
+      return (
+        exception.message || statusMessages[status] || 'Неизвестная ошибка'
+      );
+    }
+    // неизвестные исключения
+    else {
+      return statusMessages[status] || 'Неизвестная ошибка';
+    }
   }
 }
