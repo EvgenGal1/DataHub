dc1482d3aa24ef556e07a9e763212ab38b64728e
EvGen Gal <ZBst.1@yandex.ru>
2025-01-05 19:22:31 +0300

    SRV. ~ jest (в CommonJS) - ! Vercel

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,13 +1,16 @@
-export const preset = 'ts-jest';
-export const testEnvironment = 'node';
-export const transform = {
-  '^.+\\.tsx?$': 'ts-jest', // преобразов.ф.с расшир. .ts/tsx
-};
-export const testPathIgnorePatterns = ['/node_modules/', '/dist/'];
-export const globals = {
-  'ts-jest': {
-    tsconfig: 'tsconfig.json', // конфиг
+module.exports = {
+  preset: 'ts-jest', // поддержка TS
+  testEnvironment: 'node', // среда выполнения
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest', // преобразов.ф.с расшир. .ts/tsx
   },
+  // игнор.пути
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json', // конфиг
+    },
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$', //  регул.выраж. > нахождения тестов
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'], // поддержка расширений
 };
-export const testRegex = '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$';
-export const moduleFileExtensions = ['ts', 'js', 'json', 'node'];
